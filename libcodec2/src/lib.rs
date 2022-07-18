65eeda16498b2fb2ec4aaabf474843cac6771d26
Yuval Adam <_@yuv.al>
2022-07-18 08:27:42 +0300

    Add cross-dependency

--- libcodec2/src/lib.rs
+++ libcodec2/src/lib.rs
@@ -1,7 +1,11 @@
+extern crate codec2_sys;
+
 #[cfg(test)]
 mod tests {
+    use codec2_sys::CODEC2;
     #[test]
     fn it_works() {
+        let _c: CODEC2;
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
